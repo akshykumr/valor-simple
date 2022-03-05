07100abc4779f22abcd25a1b19b0a3d57fad34f6
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2022-03-06 04:32:36 +0530

    Added auto seen msgs

--- valor.js
+++ valor.js
@@ -68,7 +68,7 @@ module.exports = valor = async (valor, m, chatUpdate, store) => {
 
         // Push Message To Console && Auto Read
         if (m.message) {
-            
+            valor.sendReadReceipt(m.chat, m.sender, [m.key.id])
             console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> Of'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
         }
 
